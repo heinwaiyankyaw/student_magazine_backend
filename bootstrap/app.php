e469c572c86226cc3eee10ccfa72f8020b8d838c
waiyan <heinwaiyankyaw2001@gmail.com>
2025-02-12 20:57:03 +0700

    [add] Project Setup and Routes

---
+++ bootstrap/app.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+        using: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/hwyk/api.php'));
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/mhma/api.php'));
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/mt/api.php'));
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/wyp/api.php'));
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/yzl/api.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/hwyk/api.php'));
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/mhma/api.php'));
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/mt/api.php'));
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/wyp/api.php'));
+            Route::middleware('web')
+                ->prefix('web')
+                ->group(base_path('routes/yzl/api.php'));
+
+        }
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
---
+++ routes/mt/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('mt', function () {
+    return 'mt web';
+});
